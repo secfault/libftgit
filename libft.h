12f133eb0611ae3ac55aedfc630056fad444eccb
secfault <dumont_t@etna-alternance.net>
2018-11-21 18:12:35 +0100

    save pt2

--- libft.h
+++ libft.h
@@ -40,3 +40,8 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+void    *ft_memalloc(size_t size);
+void    ft_memdel(void **ap);
+char    *ft_strnew(size_t size);
+void    ft_strdel(char **as);
+void    ft_strclr(char *s);
