0cdbe2c68467c3d06d3da895f56789099f264836
Dumont TONY <dtony@e2r9p16.42.fr>
2018-12-08 14:49:55 +0100

    clean miss 7ex

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: dtony <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 17:50:50 by dtony             #+#    #+#             */
-/*   Updated: 2018/12/05 20:29:48 by dtony            ###   ########.fr       */
+/*   Updated: 2018/12/08 14:48:27 by dtony            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ char	*ft_itoa(int n)
 	int		ng;
 	char	*itoa;
 
-	i = -1;
 	ng = 0;
 	if (n < 0)
 	{
@@ -28,12 +27,13 @@ char	*ft_itoa(int n)
 	if (!(itoa = (char *)malloc(sizeof(char) * (ft_itoa_sizer(n) + 1))))
 		return (NULL);
 	if (ng == 1)
-		itoa[++i] = '-';
-	while ((size_t)++i < ft_strlen(itoa))
+		itoa[0] = '-';
+	i = (ng == 1) ? ft_itoa_sizer(n) + 1 : ft_itoa_sizer(n);
+	itoa[i + 1] = '\0';
+	while (n)
 	{
 		itoa[i] = n % 10 + '0';
 		n /= 10;
 	}
-	itoa[++i] = '\0';
 	return (itoa);
 }
